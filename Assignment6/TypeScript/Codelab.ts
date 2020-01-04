407aa04c35988db076c89ba0d0ca5c93885d9ba7
Raghu <jsru2@pdx.edu>
2020-01-04 06:34:00 -0800

    adding assignment6

---
+++ Assignment6/TypeScript/Codelab.ts
@@ -0,0 +1,9 @@
+import {Guest} from './Guest';
+
+export class Codelab {
+  constructor(public guests: Guest[]) {}
+
+  getGuestsComing() {
+    return this.guests.filter(guest => guest.coming);
+  }
+}
